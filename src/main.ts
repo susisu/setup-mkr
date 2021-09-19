0366c67b5359d48a6debf107319d5122a6b5abf8
Susisu <susisu@hatena.ne.jp>
2021-09-19 17:43:27 +0900

    feat: create outline of implementation

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,28 @@
 import * as core from "@actions/core";
 
-async function run(): Promise<void> {
-  const version = core.getInput("version");
-  core.info(`Setup mkr (version = ${version})`);
+export async function run(): Promise<void> {
+  try {
+    const version = core.getInput("version");
+    core.info(`Setup mkr (version = ${version})`);
+
+    core.startGroup("download");
+    await download();
+    core.endGroup();
+
+    core.startGroup("extract");
+    await extract();
+    core.endGroup();
+
+    core.startGroup("install");
+    await install();
+    core.endGroup();
+  } catch (err: unknown) {
+    core.setFailed(String(err));
+  }
 }
 
-run().catch(err => {
-  core.setFailed(String(err));
-});
+async function download(): Promise<void> {}
+
+async function extract(): Promise<void> {}
+
+async function install(): Promise<void> {}
