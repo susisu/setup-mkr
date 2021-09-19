a17d8830d756729c3d69dca1ae8dc4859da00b17
Susisu <susisu@hatena.ne.jp>
2021-09-19 16:34:02 +0900

    chore: setup eslint

---
+++ .eslintrc.js
@@ -0,0 +1,51 @@
+"use strict";
+
+module.exports = {
+  overrides: [
+    {
+      files: ["*.ts"],
+      extends: [
+        "@susisu/eslint-config/preset/ts-types",
+        "prettier",
+        "plugin:eslint-comments/recommended",
+      ],
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: "module",
+        project: "./tsconfig.json",
+      },
+      env: {
+        es6: true,
+      },
+      rules: {
+        "eslint-comments/no-unused-disable": "error",
+      },
+    },
+    {
+      files: ["*.spec.ts", "src/**/__tests__/**/*.ts"],
+      extends: ["plugin:jest/recommended", "plugin:jest-formatting/recommended"],
+      env: {
+        "jest/globals": true,
+      },
+    },
+    {
+      files: ["*.js"],
+      extends: [
+        "@susisu/eslint-config/preset/es",
+        "plugin:eslint-comments/recommended",
+        "prettier",
+      ],
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: "script",
+      },
+      env: {
+        es6: true,
+        node: true,
+      },
+      rules: {
+        "eslint-comments/no-unused-disable": "error",
+      },
+    },
+  ],
+};
