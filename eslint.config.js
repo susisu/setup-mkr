6a93109608e426c491c1946aca27b9b8a1ad621e
Susisu <susisu2413@yahoo.co.jp>
2024-02-10 16:45:21 +0900

    refactor: update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,60 +1,35 @@
-import { config, map } from "@susisu/eslint-config";
-import prettierConfig from "eslint-config-prettier";
+import { make } from "@susisu/eslint-config";
 import vitestPlugin from "eslint-plugin-vitest";
 import globals from "globals";
 
-export default [
-  ...map(
-    {
-      files: ["src/**/*.ts"],
+export default make({}, [
+  {
+    plugins: {
+      vitest: vitestPlugin,
     },
-    [
-      config.tsTypeChecked(),
-      {
-        languageOptions: {
-          sourceType: "module",
-          parserOptions: {
-            project: "./tsconfig.json",
-          },
-          globals: {
-            ...globals.es2021,
-            ...globals.node,
-          },
-        },
+  },
+  {
+    files: ["src/**/*.ts"],
+    languageOptions: {
+      globals: {
+        ...globals.es2021,
+        ...globals.node,
       },
-    ],
-  ),
-  ...map(
-    {
-      files: ["src/**/*.spec.ts", "src/**/__tests__/**/*.ts"],
     },
-    [
-      {
-        plugins: {
-          vitest: vitestPlugin,
-        },
-        rules: {
-          ...vitestPlugin.configs.recommended.rules,
-        },
-      },
-    ],
-  ),
-  ...map(
-    {
-      files: ["*.js"],
+  },
+  {
+    files: ["src/**/*.spec.ts", "src/**/__tests__/**/*.ts"],
+    rules: {
+      ...vitestPlugin.configs.recommended.rules,
     },
-    [
-      config.js(),
-      {
-        languageOptions: {
-          sourceType: "module",
-          globals: {
-            ...globals.es2021,
-            ...globals.node,
-          },
-        },
+  },
+  {
+    files: ["*.js"],
+    languageOptions: {
+      globals: {
+        ...globals.es2021,
+        ...globals.node,
       },
-    ],
-  ),
-  prettierConfig,
-];
+    },
+  },
+]);
